429f1c693bfabd86e61c9bc56fd4c8f82a7af9be
ykiu <32252655+ykiu@users.noreply.github.com>
2018-07-08 15:27:43 +0900

    Simplify the search field

--- src/components/TaxonSearch.js
+++ src/components/TaxonSearch.js
@@ -1,54 +1,16 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
-import SearchIcon from '@material-ui/icons/Search';
 import Input from '@material-ui/core/Input';
 
-const styles = {
-  root: {
-    flexGrow: 1,
-  },
-  flex: {
-    flex: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-  input: {
-    color: "white"
-    
-  },
-};
 
-function ButtonAppBar(props) {
-  const { classes } = props;
+export default function TaxonSearch(props) {
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-            <CloseIcon />
-          </IconButton>
-          <Input
-            placeholder="分類群の名前を入力"
-            inputProps={{
-              'aria-label': 'Search',
-            }}
-            className={classes.input}
-            fullWidth={true}
-            onChange={props.onSearchFieldChange}
-          />
-          <IconButton color="inherit" aria-label="Search">
-            <SearchIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-    </div>
+      <Input
+        placeholder="分類群の名前を入力"
+        inputProps={{
+          'aria-label': 'Search',
+        }}
+        fullWidth={true}
+        onChange={props.onSearchFieldChange}
+      />
   );
 }
-
-export default withStyles(styles)(ButtonAppBar);
