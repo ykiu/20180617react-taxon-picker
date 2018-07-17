d80b4136824f9a57d88b2ae72a98be66d8f8e8cb
ykiu <32252655+ykiu@users.noreply.github.com>
2018-07-17 19:19:56 +0900

    Move some code from a component to its container

--- src/components/TaxonSelection.js
+++ src/components/TaxonSelection.js
@@ -1,4 +1,4 @@
-import {Set, Map, List} from 'immutable'
+import {Set} from 'immutable'
 
 import React, { Component } from 'react';
 
@@ -6,7 +6,6 @@ import Drawer from '@material-ui/core/Drawer';
 
 import TaxonDrawerContainer from '../containers/TaxonDrawerContainer';
 import TaxonListFilteredBySearchText from '../containers/TaxonListFilteredBySearchText';
-import WithIndex from '../containers/WithIndex';
 import TaxonSearch from './TaxonSearch';
 import TaxonListAndImportPrompt from './TaxonListAndImportPrompt'
 import EditableTaxonList from './EditableTaxonList'
@@ -65,28 +64,7 @@ class TaxonSelection extends Component {
     this.setState({
       viewType: VIEW_TYPES.IMPORT_VIEW
     })
-
-    /**@type {Array<Map>} */
-    let nodeStack = this.props.referentialChildTaxaByParentIDs.get(null, List()).toArray();
-    const IDs = [];
-
-    while (nodeStack.length) {
-      const currentNode = nodeStack.pop();
-      const currentNodeID = currentNode.get('id')
-      const childNode = this.props.referentialChildTaxaByParentIDs.getIn([currentNodeID, 0]);
-      const referentialCommonNames = this.props.referentialCommonNamesByTaxonIDs.get(currentNodeID).map(obj => obj.get('name'));
-      const isExisting = (Set(this.props.personalCommonNamesByNames.keySeq()).intersect(referentialCommonNames.toSet()).count()) !== 0;
-
-      if (!isExisting) {
-        IDs.push(currentNodeID)
-      }
-
-      if (childNode !== undefined) {
-        nodeStack.push(childNode);
-      }
-    }
-
-    this.props.selectMultipleReferentialTaxa(IDs);
+    this.props.selectRelevantReferentialTaxa();
   }
 
   render() {
--- src/containers/TaxonSelectionContainer.js
+++ src/containers/TaxonSelectionContainer.js
@@ -1,3 +1,5 @@
+import {Set, Map, List} from 'immutable'
+
 import {
   changeSearchText,
   toggleReferentialTaxon,
@@ -5,6 +7,30 @@ import {
 } from '../actions/ui'
 import { connect } from 'react-redux'
 import TaxonSelection from '../components/TaxonSelection'
+
+function selectRelevantReferentialTaxa(props) {
+  /**@type {Array<Map>} */
+  let nodeStack = props.referentialChildTaxaByParentIDs.get(null, List()).toArray();
+  const IDs = [];
+
+  while (nodeStack.length) {
+    const currentNode = nodeStack.pop();
+    const currentNodeID = currentNode.get('id')
+    const childNode = props.referentialChildTaxaByParentIDs.getIn([currentNodeID, 0]);
+    const referentialCommonNames = props.referentialCommonNamesByTaxonIDs.get(currentNodeID).map(obj => obj.get('name'));
+    const isExisting = (Set(props.personalCommonNamesByNames.keySeq()).intersect(referentialCommonNames.toSet()).count()) !== 0;
+
+    if (!isExisting) {
+      IDs.push(currentNodeID)
+    }
+    if (childNode !== undefined) {
+      nodeStack.push(childNode);
+    }
+  }
+
+  return selectMultipleReferentialTaxa(IDs);
+}
+
 
 function makeMapStateToProps(state, props) {
   return {
@@ -14,10 +40,11 @@ function makeMapStateToProps(state, props) {
   }
 }
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch, ownProps) => ({
   changeSearchText: text => dispatch(changeSearchText(text)),
   toggleReferentialTaxon: taxonID => dispatch(toggleReferentialTaxon(taxonID)),
   selectMultipleReferentialTaxa: taxonIDs => dispatch(selectMultipleReferentialTaxa(taxonIDs)),
+  selectRelevantReferentialTaxa: () => dispatch(selectRelevantReferentialTaxa(ownProps))
 });
 
 export default this.container = connect(
